3ded18b19f71a080fbe131247011eed6eb022842
KJB <drv98@naver.com>
2026-06-25 16:17:51 +0900

    할일폼 CSS 완료

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,3 +1,5 @@
+import "./TaskForm.css";
+
 export default function TaskForm() {
   return (
     <header className="app_header">
@@ -5,23 +7,26 @@ export default function TaskForm() {
         <input
           type="text"
           className="task_input"
-          placeholder="Enter your task"
+          placeholder="할일을 입력하세요"
         />
 
         <div className="task_form_bottom_line">
-          <button className="tag">HTML</button>
-          <button className="tag">CSS</button>
-          <button className="tag">JavaScript</button>
-          <button className="tag">React</button>
-
-          <select className="task_status">
-            <option value="todo">할일</option>
-            <option value="todo">진행중</option>
-            <option value="todo">완료</option>
-          </select>
-          <button type="submit" className="task_submit">
-            + 추가
-          </button>
+          <div>
+            <button className="tag">HTML</button>
+            <button className="tag">CSS</button>
+            <button className="tag">JavaScript</button>
+            <button className="tag">React</button>
+          </div>
+          <div>
+            <select className="task_status">
+              <option value="todo">할일</option>
+              <option value="todo">진행중</option>
+              <option value="todo">완료</option>
+            </select>
+            <button type="submit" className="task_submit">
+              + 추가
+            </button>
+          </div>
         </div>
       </form>
     </header>
