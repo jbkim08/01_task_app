49c05dcdb1820cbf11839c9fdf6ab6be72c52be3
KJB <drv98@naver.com>
2026-06-25 16:11:31 +0900

    할일폼 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import TaskForm from "./components/TaskForm";
 
 export default function App() {
   return (
     <div className="app">
-      <header className="app_header">Header Section</header>
+      <TaskForm />
       <main className="app_main">
         <section className="task_column">section 1</section>
         <section className="task_column">section 2</section>
