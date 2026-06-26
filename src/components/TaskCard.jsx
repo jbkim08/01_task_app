54c94b10082d050e207a3f2931e98a2db50e5af9
KJB <drv98@naver.com>
2026-06-26 12:14:22 +0900

    삭제하기전 물어보기

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -3,6 +3,9 @@ import Tag from "./Tag";
 import deleteIcon from "../assets/delete.png";
 
 export default function TaskCard({ title, tags, handleDelete, index }) {
+  const handleClick = () => {
+    if (confirm("삭제할까요?")) handleDelete(index);
+  };
   return (
     <article className="task_card">
       <p className="task_text">{title}</p>
@@ -13,7 +16,7 @@ export default function TaskCard({ title, tags, handleDelete, index }) {
             <Tag key={index} tagName={tag} selected={true} disabled />
           ))}
         </div>
-        <div className="task_delete" onClick={() => handleDelete(index)}>
+        <div className="task_delete" onClick={handleClick}>
           <img className="delete_icon" src={deleteIcon} alt="" />
         </div>
       </div>
